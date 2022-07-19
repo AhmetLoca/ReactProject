0e6b6aa4c742cfb2377ef7ad2f7036aff34309eb
ahmetloca <locaahmet@gmail.com>
2022-07-19 15:24:00 +0300

    filtreleme islemi yapildi

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import List from './List'
 import Form from './Form'
 
 function Contacts() {
+  const [contacts, setContacts] = useState([
+    {
+      fullname: 'Mehmet',
+      phone_number: '1234'
+    },
+    {
+      fullname: 'Ahmet',
+      phone_number: '9876'
+    },
+    {
+      fullname: 'Ayşe',
+      phone_number: '2345'
+    }
+
+  ]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts])
   return (
     <div>
-      <List />
-      <Form />
+      <List contacts={contacts} />
+      <Form addContact={setContacts} contacts={contacts} />
     </div>
   )
-
 }
-
 export default Contacts;
